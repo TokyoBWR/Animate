b87367e8e0b13f239e36562b307d6c7b31ee8eb2
Жегев Реев <93246335+TokyoBWR@users.noreply.github.com>
2023-03-27 12:20:46 +0300

    Add files via upload

---
+++ Animate/Animate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookBook
+//
+//  Created by Егоров Алексей Михайлович on 3/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
